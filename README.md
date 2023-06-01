5e066c158b82e159874451b1a7c6100b41769544
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-06-01 17:11:07 +0200

    Add grid, grid generation slider and Hover
    Added in grid generation slider between 8x8 and 64x64.
    Creates the grid within #container made up of rows and blocks.
    Added hover over blocks.

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ create inital layout in html/css
     create bar for buttons
 link java.js
 create loop function for creating divs 
+    >Get divs to stay in their box
 check if it works with different dimensions
 create .hover effect with opacity + 0.25 each time
--- java.js
+++ java.js
@@ -1,1 +1,38 @@
 //JavaScript
+
+parent = document.querySelector(".container")
+
+const row = document.createElement('div')
+const block = document.createElement('div')
+block.className = "block"
+row.className = "row"
+
+//number = 16;
+
+const slider = document.getElementById("gridSize");
+const output1 = document.getElementById("demo1");
+const output2 = document.getElementById("demo2")
+output1.innerHTML = slider.value;
+output2.innerHTML = slider.value;
+
+
+slider.oninput = function() {
+    number = this.value;
+    output1.innerHTML = this.value;
+    output2.innerHTML = this.value;
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }   
+    for (let i = 0; i < number; i++){
+        const row = document.createElement('div');
+        row.className = `row ${i+1}`;
+        parent.appendChild(row);
+        for (let j = 0; j < number; j++){
+            const block = document.createElement('div');
+            block.className = "block";
+            row.appendChild(block)
+        }  
+    } 
+}
+
+
